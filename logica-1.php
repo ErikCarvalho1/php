0331eedb36f4f30e6e8263dc743a6171726aee74
ERIK CARVALHO DA SILVA <erik.csilva7@senacsp.edu.br>
2025-09-04 11:52:22 -0300

    continua

---
+++ logica-1.php
@@ -0,0 +1,78 @@
+
+ <!-- Exercicio 1 -->
+<?php
+
+$nome = "Erik Carvalho";
+echo $nome;
+
+?>
+
+<!-- exercicio 2 -->
+
+<?php 
+$idade = 20;
+echo "eu tenho: ".$idade." anos"
+?>
+
+<!-- Exercio 3 -->
+<?php 
+$n1 = 10;
+$n2 = 5;
+echo "$n1 + $n2 = ". $n1+$n2 ;
+?>
+
+<!-- exercio 4 -->
+ <?php 
+ $cidade = "São Paulo";
+ $pais = "Brasil";
+ echo "eu moro na cidade $cidade, $pais";
+ ?>
+ <!-- exercicio 5  -->
+<?php 
+define("TJ", "0.05");
+$capital = 1000;
+$valor_juros = TJ * $capital;
+echo $valor_juros;
+?>
+<!-- exercicio 6 -->
+<?php 
+$temp = 25;
+echo "A temperatura de hoje é $temp °C "
+?>
+<!-- exercicio 7 -->
+<?php 
+$primeiroNome = "Carvalho";
+$sobrenomeNome = "Silva"
+$nomeCompleto = 
+echo er
+?>
+
+<!-- exercicio 8 -->
+ <?php 
+ define("PI","3.14159");
+ $raio = 5;
+ $area = PI * $raio * $raio;
+ echo $area;
+
+ ?>
+
+
+
+<!-- exercicio 9 -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
